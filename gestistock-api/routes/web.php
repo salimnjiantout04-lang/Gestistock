fdb46669c1d1a1c0388b868b780d4769b043ab8f
salimnjiantout04-lang <salimnjiantout04-lang@users.noreply.github.com>
2026-08-24 14:22:57 +0100

    Endpoint sante JSON + Dockerfile pour deploiement Render

--- gestistock-api/routes/web.php
+++ gestistock-api/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'app'    => config('app.name', 'GestiStock'),
+        'status' => 'ok',
+        'time'   => now()->toIso8601String(),
+    ]);
 });
